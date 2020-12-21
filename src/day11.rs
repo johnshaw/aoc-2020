662a3d1ed292bef04993c68150a294f8cecaa96d
John Shaw <jshaw@shawnation.com>
2020-12-20 20:21:24 -0600

    day 11 part b

--- src/day11.rs
+++ src/day11.rs
@@ -16,20 +16,42 @@ impl Seats {
         }
     }
 
+    fn total_occupied(&self) -> usize {
+        self.seats.iter().filter(|&&c| c == '#').count()
+    }
+
+    fn next_state(&self, rule: fn(&Seats, usize) -> char) -> Seats {
+        Seats {
+            width: self.width,
+            seats: (0..self.seats.len()).map(|i| rule(&self, i)).collect(),
+        }
+    }
+}
+
+fn solve(seats: Seats, rule: fn(&Seats, usize) -> char) -> Seats {
+    std::iter::successors(Some(seats), |s| Some(s.next_state(rule)))
+        .tuple_windows()
+        .find(|(a, b)| *a == *b)
+        .unwrap()
+        .0
+}
+
+fn rule_parta(seats: &Seats, pos: usize) -> char {
     // Return neighbors in clockwise order
-    fn neighbors(&self, i: i64) -> [char; 8] {
+    let neighbors = |i: i64| {
         let sget = |row, col| {
-            if row < 0 || col < 0 || col >= self.width {
+            if row < 0 || col < 0 || col >= seats.width {
                 '.'
             } else {
-                self.seats
-                    .get((row * self.width + col) as usize)
+                seats
+                    .seats
+                    .get((row * seats.width + col) as usize)
                     .copied()
                     .unwrap_or('.')
             }
         };
-        let row = i / self.width;
-        let col = i - row * self.width;
+        let row = i / seats.width;
+        let col = i - row * seats.width;
         [
             sget(row - 1, col - 1),
             sget(row - 1, col),
@@ -40,44 +62,64 @@ impl Seats {
             sget(row + 1, col - 1),
             sget(row, col - 1),
         ]
-    }
+    };
 
-    fn total_occupied(&self) -> usize {
-        self.seats.iter().filter(|&&c| c == '#').count()
-    }
+    let adj_occupied = |i: i64| neighbors(i).iter().filter(|&&c| c == '#').count();
 
-    fn adj_occupied(&self, i: i64) -> usize {
-        self.neighbors(i).iter().filter(|&&c| c == '#').count()
-    }
-
-    fn next_state(&self) -> Seats {
-        Seats {
-            width: self.width,
-            seats: self
-                .seats
-                .iter()
-                .enumerate()
-                .map(|(i, &c)| match c {
-                    'L' if self.adj_occupied(i as i64) == 0 => '#',
-                    '#' if self.adj_occupied(i as i64) > 3 => 'L',
-                    s => s,
-                })
-                .collect(),
-        }
+    match seats.seats[pos] {
+        'L' if adj_occupied(pos as i64) == 0 => '#',
+        '#' if adj_occupied(pos as i64) > 3 => 'L',
+        s => s,
     }
 }
 
 pub fn part_a(data: String) -> Result<usize, Error> {
     let seats = Seats::new(&data);
-    let final_state = std::iter::successors(Some(seats), |s| Some(s.next_state()))
-        .map(std::rc::Rc::new)
-        .tuple_windows()
-        .find(|(a, b)| *a == *b)
-        .unwrap()
-        .0;
-    Ok(final_state.total_occupied())
+    Ok(solve(seats, rule_parta).total_occupied())
 }
 
-pub fn part_b(data: String) -> Result<i64, Error> {
-    Ok(0)
+fn rule_partb(seats: &Seats, pos: usize) -> char {
+    // Return neighbors in clockwise order
+    let neighbors = |i: i64| {
+        let search = |ir, ic| {
+            let mut row = i / seats.width;
+            let mut col = i - row * seats.width;
+            loop {
+                row += ir;
+                col += ic;
+                if row < 0 || col < 0 || col >= seats.width {
+                    break '.'
+                } else {
+                    match seats.seats.get((row * seats.width + col) as usize).copied() {
+                        Some(c) if c != '.' => break c,
+                        None => break '.',
+                        _ => {},
+                    }
+                }
+            }
+        };
+        [
+            search(-1, -1),
+            search(-1, 0),
+            search(-1, 1),
+            search(0, 1),
+            search(1, 1),
+            search(1, 0),
+            search(1, -1),
+            search(0, -1),
+        ]
+    };
+
+    let adj_occupied = |i: i64| neighbors(i).iter().filter(|&&c| c == '#').count();
+
+    match seats.seats[pos] {
+        'L' if adj_occupied(pos as i64) == 0 => '#',
+        '#' if adj_occupied(pos as i64) > 4 => 'L',
+        s => s,
+    }
+}
+
+pub fn part_b(data: String) -> Result<usize, Error> {
+    let seats = Seats::new(&data);
+    Ok(solve(seats, rule_partb).total_occupied())
 }
